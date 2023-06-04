bc3607cffd51dc64eaac47a4cfd37ee593096ac7
Nicolas Pulido <npulido@zonehacks.net>
2023-06-03 19:24:02 -0700

    just white window

---
+++ generate_js.sh
@@ -0,0 +1,9 @@
+rm -f web/worker.dart.js
+rm -f web/worker.dart.min.js
+dart run build_runner build --delete-conflicting-outputs -o web:build/web/
+cp -f build/web/worker.dart.js web/worker.dart.js
+rm -rf build/web
+dart run build_runner build --release --delete-conflicting-outputs -o web:build/web/
+cp -f build/web/worker.dart.js web/worker.dart.min.js
+rm -rf build/web
+
--- lib/db/models.dart
+++ lib/db/models.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'dart:async';
 import 'dart:core';
 import 'package:drift/drift.dart';
-import 'package:drift/native.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
